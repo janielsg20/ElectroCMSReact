8af02379335ce69d2815fb7b8db9311b5da1bad6
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-08-08 04:04:21 -0400

    feat: expose MF-042 advanced field runtime

--- src/core/content/index.ts
+++ src/core/content/index.ts
@@ -3,17 +3,15 @@ export * from './taxonomy';
 export * from './field-type-definition';
 export * from './field-type-registry';
 export * from './builtin-field-types';
+export * from './advanced-field-runtime';
+export * from './advanced-field-types';
 export {
   CUSTOM_FIELD_DEFINITION_VERSION,
   FIELD_GROUP_DEFINITION_VERSION,
   createDefaultCustomFieldDefinition,
   createDefaultFieldGroupDefinition,
-  createFieldGroup,
-  listFieldGroupDefinitions,
   serializeCustomFieldDefinition,
   serializeFieldGroupDefinition,
-  updateFieldGroup,
-  validateFieldGroupDefinition,
   type CustomFieldDefinition,
   type FieldGroupDefinition,
   type FieldGroupMutationError,
@@ -24,6 +22,16 @@ export {
   type FieldGroupValidationIssue,
   type FieldGroupValidationResult,
 } from './field-group';
+export {
+  createAdvancedFieldGroup,
+  createAdvancedFieldGroup as createFieldGroup,
+  listAdvancedFieldGroupDefinitions,
+  listAdvancedFieldGroupDefinitions as listFieldGroupDefinitions,
+  updateAdvancedFieldGroup,
+  updateAdvancedFieldGroup as updateFieldGroup,
+  validateAdvancedFieldGroupDefinition,
+  validateAdvancedFieldGroupDefinition as validateFieldGroupDefinition,
+} from './advanced-field-group';
 export {
   removeFieldGroupWithRecordIntegrity,
   removeFieldGroupWithRecordIntegrity as removeFieldGroup,
