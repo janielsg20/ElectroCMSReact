00663f5313cf90633645a0501f6621ec3b54eef2
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-08-08 03:46:09 -0400

    refactor: route field group removal through record integrity guard

--- src/core/content/index.ts
+++ src/core/content/index.ts
@@ -3,6 +3,29 @@ export * from './taxonomy';
 export * from './field-type-definition';
 export * from './field-type-registry';
 export * from './builtin-field-types';
-export * from './field-group';
-export * from './field-group-record-integrity';
+export {
+  CUSTOM_FIELD_DEFINITION_VERSION,
+  FIELD_GROUP_DEFINITION_VERSION,
+  createDefaultCustomFieldDefinition,
+  createDefaultFieldGroupDefinition,
+  createFieldGroup,
+  listFieldGroupDefinitions,
+  serializeCustomFieldDefinition,
+  serializeFieldGroupDefinition,
+  updateFieldGroup,
+  validateFieldGroupDefinition,
+  type CustomFieldDefinition,
+  type FieldGroupDefinition,
+  type FieldGroupMutationError,
+  type FieldGroupMutationErrorCode,
+  type FieldGroupMutationResult,
+  type FieldGroupPresentation,
+  type FieldGroupValidationCode,
+  type FieldGroupValidationIssue,
+  type FieldGroupValidationResult,
+} from './field-group';
+export {
+  removeFieldGroupWithRecordIntegrity,
+  removeFieldGroupWithRecordIntegrity as removeFieldGroup,
+} from './field-group-record-integrity';
 export * from './content-record';
