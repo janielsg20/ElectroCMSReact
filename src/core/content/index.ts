c6811401424d9fc343a8bf247a8d059a641d4b4b
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-08-08 02:32:24 -0400

    feat: export field type registry contracts

--- src/core/content/index.ts
+++ src/core/content/index.ts
@@ -1,2 +1,5 @@
 export * from './content-type';
 export * from './taxonomy';
+export * from './field-type-definition';
+export * from './field-type-registry';
+export * from './builtin-field-types';
