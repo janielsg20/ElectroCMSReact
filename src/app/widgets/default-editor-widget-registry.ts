0bd32dcbae11b7e6926445fa18c339006bc112eb
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-08-07 22:13:08 -0400

    feat: include modeled dynamic widget contracts

--- src/app/widgets/default-editor-widget-registry.ts
+++ src/app/widgets/default-editor-widget-registry.ts
@@ -1,4 +1,5 @@
 import { registerBasicContentWidgets } from './core-basic-content-widgets';
+import { registerDynamicContractWidgets } from './core-dynamic-contract-widgets';
 import { registerStructuralWidgets } from './core-structural-widgets';
 import { EditorWidgetRegistry } from './editor-widget-registry';
 
@@ -6,6 +7,7 @@ export function createDefaultEditorWidgetRegistry(): EditorWidgetRegistry {
   const registry = new EditorWidgetRegistry();
   registerStructuralWidgets(registry);
   registerBasicContentWidgets(registry);
+  registerDynamicContractWidgets(registry);
   return registry;
 }
 
