99618c41af05c307d3891e795697ed090f8c1ae0
janielsg20 <72001897+janielsg20@users.noreply.github.com>
2026-08-09 08:48:36 -0700

    feat(f05): export field type registry APIs

--- src/core/content/index.ts
+++ src/core/content/index.ts
@@ -1,2 +1,5 @@
+export * from './builtin-field-types';
 export * from './content-type';
+export * from './field-type-definition';
+export * from './field-type-registry';
 export * from './taxonomy';
